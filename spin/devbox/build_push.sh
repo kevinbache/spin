8c6ed7bb8a2dda624dc66790ad269d035e313ce5
Kevin Bache <kevin.bache@gmail.com>
2019-07-18 17:46:41 -0700

    removed devbox

--- spin/devbox/build_push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ../..
-export PROJECT_ID=kb-experiment
-export IMAGE_REPO_NAME=devbox
-export IMAGE_TAG=latest
-export IMAGE_URI="gcr.io/${PROJECT_ID}/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
-docker build -f spin/devbox/Dockerfile -t ${IMAGE_URI} .
-docker push ${IMAGE_URI}
